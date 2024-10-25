8bcb3f2048981ece371ccfd20f218993f80b8d9d
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2024-10-25 09:41:55 +0200

    chore: Add test for health handler

---
+++ dao/inmemory_mock_impl.go
@@ -0,0 +1,84 @@
+package dao
+
+import (
+	"context"
+	"fmt"
+	daoErr "github.com/go-feature-flag/app-api/dao/err"
+	"github.com/go-feature-flag/app-api/model"
+	_ "github.com/lib/pq" // we import the driver used by sqlx
+)
+
+func NewInMemoryMockDao() (*InMemoryMockDao, error) {
+	return &InMemoryMockDao{
+		flags: []model.FeatureFlag{},
+	}, nil
+}
+
+type InMemoryMockDao struct {
+	flags []model.FeatureFlag
+
+	errorOnPing bool
+}
+
+// GetFlags return all the flags
+func (m *InMemoryMockDao) GetFlags(ctx context.Context) ([]model.FeatureFlag, daoErr.DaoError) {
+	return m.flags, nil
+}
+
+// GetFlagByID return a flag by its ID
+func (m *InMemoryMockDao) GetFlagByID(ctx context.Context, id string) (model.FeatureFlag, daoErr.DaoError) {
+	for _, flag := range m.flags {
+		if flag.ID == id {
+			return flag, nil
+		}
+	}
+	return model.FeatureFlag{}, daoErr.NewDaoError(daoErr.NotFound, fmt.Errorf("flag with id %s not found", id))
+}
+
+// GetFlagByName return a flag by its name
+func (m *InMemoryMockDao) GetFlagByName(ctx context.Context, name string) (model.FeatureFlag, daoErr.DaoError) {
+	for _, flag := range m.flags {
+		if flag.Name == name {
+			return flag, nil
+		}
+	}
+	return model.FeatureFlag{}, daoErr.NewDaoError(daoErr.NotFound, fmt.Errorf("flag with name %s not found", name))
+}
+
+// CreateFlag create a new flag, return the id of the flag
+func (m *InMemoryMockDao) CreateFlag(ctx context.Context, flag model.FeatureFlag) (string, daoErr.DaoError) {
+	m.flags = append(m.flags, flag)
+	return flag.ID, nil
+}
+
+func (m *InMemoryMockDao) UpdateFlag(ctx context.Context, flag model.FeatureFlag) daoErr.DaoError {
+	for index, f := range m.flags {
+		if f.ID == flag.ID {
+			m.flags[index] = flag
+			return nil
+		}
+	}
+	return daoErr.NewDaoError(daoErr.NotFound, fmt.Errorf("flag with id %s not found", flag.ID))
+}
+
+func (m *InMemoryMockDao) DeleteFlagByID(ctx context.Context, id string) daoErr.DaoError {
+	newInmemoryFlagList := []model.FeatureFlag{}
+	for _, f := range m.flags {
+		if f.ID != id {
+			newInmemoryFlagList = append(newInmemoryFlagList, f)
+		}
+	}
+	m.flags = newInmemoryFlagList
+	return nil
+}
+
+func (m *InMemoryMockDao) Ping() daoErr.DaoError {
+	if m.errorOnPing {
+		return daoErr.NewDaoError(daoErr.DatabaseNotInitialized, fmt.Errorf("error on ping"))
+	}
+	return nil
+}
+
+func (m *InMemoryMockDao) OnPingReturnError(v bool) {
+	m.errorOnPing = v
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/labstack/echo/v4 v4.12.0
 	github.com/lib/pq v1.10.9
+	github.com/stretchr/testify v1.9.0
 	github.com/swaggo/echo-swagger v1.4.1
 	github.com/swaggo/swag v1.16.3
 )
@@ -32,7 +33,6 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rogpeppe/go-internal v1.13.1 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/swaggo/files/v2 v2.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
@@ -43,4 +43,5 @@ require (
 	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/tools v0.22.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
