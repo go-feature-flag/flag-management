e3ef90e89666215767e0e21bcb78e6f8f94e7b6d
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2024-10-28 14:17:07 +0100

    chore: test jsonb

--- dao/dbmodel/feature_flag.go
+++ dao/dbmodel/feature_flag.go
@@ -1,24 +1,11 @@
 package dbmodel
 
 import (
-	"database/sql/driver"
-	"encoding/json"
 	"time"
 
 	"github.com/go-feature-flag/app-api/model"
 	"github.com/google/uuid"
 )
-
-type JSONB json.RawMessage
-
-func (j JSONB) Value() (driver.Value, error) {
-	valueString, err := json.Marshal(j)
-	return string(valueString), err
-}
-
-func (j *JSONB) Scan(value interface{}) error {
-	return json.Unmarshal(value.([]byte), &j)
-}
 
 type FeatureFlag struct {
 	ID              uuid.UUID      `db:"id"`
@@ -41,36 +28,26 @@ func FromModelFeatureFlag(mff model.FeatureFlag) (FeatureFlag, error) {
 	if err != nil {
 		return FeatureFlag{}, err
 	}
-
-	variations, err := json.Marshal(mff.Variations)
-	if err != nil {
-		return FeatureFlag{}, err
-	}
-
-	var metadata JSONB
-	if mff.Metadata != nil {
-		metadataBytes, err := json.Marshal(mff.Metadata)
-		if err != nil {
-			return FeatureFlag{}, err
-		}
-		metadata = JSONB(metadataBytes)
-	}
-
-	return FeatureFlag{
+	ff := FeatureFlag{
 		ID:              id,
 		Name:            mff.Name,
 		Description:     mff.Description,
-		Variations:      JSONB(variations),
 		Type:            mff.VariationType,
 		BucketingKey:    mff.BucketingKey,
-		Metadata:        metadata,
 		TrackEvents:     mff.TrackEvents,
 		Disable:         mff.Disable,
 		Version:         mff.Version,
 		CreatedDate:     mff.CreatedDate,
 		LastUpdatedDate: mff.LastUpdatedDate,
 		LastModifiedBy:  mff.LastModifiedBy,
-	}, nil
+	}
+	if mff.Variations != nil {
+		ff.Variations = JSONB(*mff.Variations)
+	}
+	if mff.Metadata != nil {
+		ff.Metadata = JSONB(*mff.Metadata)
+	}
+	return ff, nil
 }
 
 func (ff *FeatureFlag) ToModelFeatureFlag(rules []Rule) (model.FeatureFlag, error) {
@@ -87,19 +64,14 @@ func (ff *FeatureFlag) ToModelFeatureFlag(rules []Rule) (model.FeatureFlag, erro
 		}
 		apiRules = append(apiRules, convertedRule)
 	}
-
-	var variations map[string]*interface{}
-	err := json.Unmarshal(ff.Variations, &variations)
-	if err != nil {
-		return model.FeatureFlag{}, err
+	variations := make(map[string]interface{})
+	if ff.Variations != nil {
+		variations = ff.Variations
 	}
-
-	var metadata map[string]interface{}
-	err = json.Unmarshal(ff.Metadata, &metadata)
-	if err != nil {
-		return model.FeatureFlag{}, err
+	metadata := make(map[string]interface{})
+	if ff.Metadata != nil {
+		metadata = ff.Metadata
 	}
-
 	return model.FeatureFlag{
 		ID:              ff.ID.String(),
 		Name:            ff.Name,
---
+++ dao/dbmodel/jsonb.go
@@ -0,0 +1,22 @@
+package dbmodel
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+)
+
+type JSONB map[string]interface{}
+
+func (a JSONB) Value() (driver.Value, error) {
+	return json.Marshal(a)
+}
+
+func (a *JSONB) Scan(value interface{}) error {
+	b, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+
+	return json.Unmarshal(b, &a)
+}
